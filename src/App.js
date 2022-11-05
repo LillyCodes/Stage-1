d576c921d07650bb3ddaabab1814f72cc9af13fb
LilyCode <lilianwilliams219@gmail.com>
2022-11-05 21:17:12 +0100

    home page

--- src/App.js
+++ src/App.js
@@ -1,107 +1,19 @@
-import { RiMoreLine } from 'react-icons/ri';
-import { RiShareForwardLine } from 'react-icons/ri';
-import { BsGithub } from 'react-icons/bs';
-import { BsSlack } from 'react-icons/bs';
-import Avatar from './image/avatar.JPG'
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-
+import Home from "./routes/Home";
+import Contact from "./routes/Contact";
 
 function App() {
   return (
-    <>
-     <br></br>
-     <br></br>
-     <section className='profile'>
-       <div className='share'>
-         <RiShareForwardLine />
-       </div>
-       <div className='more'>
-          <RiMoreLine />
-       </div>
-
-       <div id='profile__img'>
-         <img src={Avatar} alt='Lilian Williams'/>
-       </div>
-
-       <p>Lilian Williams</p>
-       <p id='slack'>Lilian Williams</p>
-     </section>
-
-     <section className='link'>
-        <button id='twitter'>
-          <a href='https://twitter.com/LezziLily'
-          target='_blank'
-          rel='noreferrer'>
-           Twitter Link
-          </a>
-        </button>
-
-        <button id='btn__zuri'>
-          <a href="https://training.zuri.team/"
-          target='_blank'
-          rel='noreferrer'>
-           Zuri Team
-          </a>
-        </button>
-
-        <button id="books">
-          <a href="http://books.zuri.team/" target="_blank" rel="noreferrer">
-            Zuri Books
-          </a>
-        </button>
-
-        <button id="book__python">
-          <a href="https://books.zuri.team/" target="_blank" rel="noreferrer">
-            Python Books
-          </a>
-        </button>
-
-        <button id="pitch">
-          <a
-            href="https://background.zuri.team/"
-            target="_blank"
-            rel="noreferrer"
-          >
-            Background Check for Coders
-          </a>
-        </button>
-        
-
-        <button id="book__design">
-          <a
-            href="https://training.zuri.team/"
-            target="_blank"
-            rel="noreferrer"
-          >
-            Design Books
-          </a>
-        </button>
-
-        <br></br>
-        <p>
-           <a
-            id='icon'
-            href='https://github.com/LillyCodes'
-            target='_blank'
-            rel='noreferrer'
-          >
-            <BsGithub />
-          </a> 
-
-           <a
-            id='icon'
-            href='https://mail.zuri.team/l/1sw892IMOS9B69KON7AsFDQw/Tf7fWUnd763Xz1DsvpVMzLLQ/YUHM7P7OdX5hgAPlCp0Rsg'
-            target='_blank'
-            rel='noreferrer'
-          >
-            <BsSlack />
-          </a> 
-
-        </p>
-
-     </section>
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </Router>
   );
 }
 
 export default App;
+
